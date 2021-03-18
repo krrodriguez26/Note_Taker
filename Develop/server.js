79aa88aa13ceaa4320c86f71aa73b4c94f92af43
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-03-18 12:42:26 -0400

    Moved routes folder

--- Develop/server.js
+++ Develop/server.js
@@ -2,23 +2,17 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-// Express App set-up
+// Express App setup
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Data parsing set-up
+// Data parsing setup
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-
-
-
 //Route
-app.get('/',(req,res)=> {
-    res.send(app);
-});
+require('../Develop/Routes/routes')(app);
 
-app.listen(PORT, ()=>{
-    Console.log('App is listening on ${PORT}')
-});
+app.listen(PORT, () =>
+    Console.log(`App is listening on PORT ${PORT}`));
