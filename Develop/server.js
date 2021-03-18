d7b8791e2972c2e8c541c8ddeac8d244c5b3c5de
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-03-18 17:41:24 -0400

    Fixed errors on server

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const database = require("./db/db.json");
 
 // Express App setup
 const app = express();
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Data parsing setup
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
 //Route
 require('../Develop/Routes/routes')(app);
